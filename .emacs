239eaed00b72d175b522c6e78d0b55b44fe340c7
Jonathan Groll <jonathanjgster@gmail.com>
2009-10-15 12:33:39 +0200

    Restored w3m

--- .emacs
+++ .emacs
@@ -683,8 +683,8 @@ fun)))
 (require 'tail)
 
 ;;emacs-w3m
-;;(require 'w3m-load)
-;;(require 'mime-w3m)
+(require 'w3m-load)
+;; (require 'mime-w3m)
 
 ;;org mode
 (require 'org-install)
--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -26,9 +26,3 @@
  '(rails-ws:default-server-type "webrick")
  '(rdebug-many-windows nil)
  '(ruby-electric-expand-delimiters-list nil))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
